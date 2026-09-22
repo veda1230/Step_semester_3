ec04b7887cefc847c6678c2fd2dc5f4e5f61f33b
veda1230 <vv1576@srmist.edhu.in>
2026-09-22 21:09:45 +0530

    Add Rock Paper Scissors game

---
+++ RockPaperScissors.java
@@ -0,0 +1,65 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class RockPaperScissors {
+
+    static String playRound(String playerMove, String computerMove) {
+        if (playerMove.equals(computerMove)) {
+            return "Draw";
+        }
+
+        if ((playerMove.equals("Rock") && computerMove.equals("Scissors")) ||
+            (playerMove.equals("Paper") && computerMove.equals("Rock")) ||
+            (playerMove.equals("Scissors") && computerMove.equals("Paper"))) {
+            return "Player Wins";
+        }
+
+        return "Computer Wins";
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random random = new Random();
+
+        String[] moves = {"Rock", "Paper", "Scissors"};
+
+        int wins = 0;
+        int losses = 0;
+        int draws = 0;
+
+        System.out.print("Enter number of rounds: ");
+        int n = sc.nextInt();
+
+        System.out.println("\nRound | Player Move | Computer Move | Result");
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print("Enter your move (Rock/Paper/Scissors): ");
+            String playerMove = sc.next();
+
+            String computerMove = moves[random.nextInt(3)];
+
+            String result = playRound(playerMove, computerMove);
+
+            if (result.equals("Player Wins")) {
+                wins++;
+            } else if (result.equals("Computer Wins")) {
+                losses++;
+            } else {
+                draws++;
+            }
+
+            System.out.println(i + "     | " + playerMove + "       | "
+                    + computerMove + "       | " + result);
+        }
+
+        double winPercentage = (wins * 100.0) / n;
+
+        System.out.println("\nFinal Summary");
+        System.out.println("Wins: " + wins);
+        System.out.println("Losses: " + losses);
+        System.out.println("Draws: " + draws);
+        System.out.printf("Win Percentage: %.1f%%\n", winPercentage);
+
+        sc.close();
+    }
+}
